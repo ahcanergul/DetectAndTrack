4ab63d33a0da04aad136d622e6537093ae7af84b
caner <ahcaner@gmail.com>
2021-10-19 00:15:22 +0300

    caner snapshot

---
+++ mavsdk_utils.hpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <cmath>
+#include <future>
+#include <iostream>
+#include <thread>
+
+// #include <mavsdk.h>
+// #include <plugins/action/action.h>
+// #include <plugins/offboard/offboard.h>
+// #include <plugins/telemetry/telemetry.h>
+
+// using namespace mavsdk;
+// using std::chrono::milliseconds;
+// using std::chrono::seconds;
+// using std::this_thread::sleep_for;
